102e94e29b76ba7348ecd3503f875b1a42f0dc8c
najialshari <najigoon2@gmail.com>
2022-07-10 20:33:44 +0300

    correct homepage with basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/Blog2022'>
     <App />
     </BrowserRouter>
   </React.StrictMode>
